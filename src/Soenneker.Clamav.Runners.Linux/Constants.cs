e61a72e301977eed5b7bbfeee6c84b967f918180
Jake Soenneker <jake@soenneker.com>
2026-09-02 15:10:00 -0500

    Publish Linux FreshClam package

--- src/Soenneker.Clamav.Runners.Linux/Constants.cs
+++ src/Soenneker.Clamav.Runners.Linux/Constants.cs
@@ -3,5 +3,6 @@ namespace Soenneker.Clamav.Runners.Linux;
 public static class Constants
 {
     public const string Library = "Soenneker.Clamav.Linux";
+    public const string FreshclamLibrary = "Soenneker.Clamav.Freshclam.Linux";
     public const string RuntimeIdentifier = "linux-x64";
 }
